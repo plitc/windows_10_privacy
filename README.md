bd2a245184eda4f1ba7b0dbd7ffdcfb285e3bd83
daniel.plominski <daniel@plominski.eu>
2015-09-09 16:48:33 +0200

    Version: 0.1

---
+++ README.md
@@ -0,0 +1,47 @@
+
+Background
+==========
+based on:
+* IX (9 September 2015) - Browser Edge, Sicherheitsfunktionen, Datenschutz: Windows 10
+
+WARNING
+=======
+
+Dependencies
+============
+* Windows 10+
+   * [PowerShell](https://www.microsoft.com/en-us/download/details.aspx?id=34595) (optional)
+   * [Github on Windows](https://windows.github.com/) (optional)
+
+Features
+========
+
+Platform
+========
+* Windows 10+
+
+Usage
+=====
+    # ./win10_privacy.ps1
+
+Usage (for Windows)
+===================
+* run as administrator (allow the untrusted powershell scripts)
+```
+   Set-ExecutionPolicy Unrestricted
+```
+
+* run as administrator
+```
+   PS C:\github\windows_10_privacy> .\win10_privacy.ps1
+```
+
+Screencast
+==========
+
+Errata
+======
+* 09.09.2015:
+```
+```
+
---
+++ win10_privacy.ps1
@@ -0,0 +1,65 @@
+<#
+
+### LICENSE // ###
+#
+# Copyright (c) 2014-2015, Daniel Plominski (Plominski IT Consulting)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+### // LICENSE ###
+
+#>
+
+#"run as administrator"
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments
+break
+}
+
+echo '# ### ### ### ##### ### ### ### #'
+echo '#                               #'
+echo '# (\/)                          #'
+echo '# (..)   win10_privacy.ps1      #'
+echo '# (")(")                        #'
+echo '#                               #'
+echo '# ### ### ### ##### ### ### ### #'
+echo "" # dummy
+Start-Sleep -s 10
+
+echo "-> "
+
+
+
+echo "-> "
+
+
+
+echo "" # dummy
+Start-Sleep -s 5
+
+echo "" # dummy
+echo "EXIT"
+Start-Sleep -s 2
+
